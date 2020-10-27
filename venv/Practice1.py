e95d4b7e8866e3c53ca20383d5f19f9e02d04eeb
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-10-27 13:05:37 +0300

    Initial commit

---
+++ venv/Practice1.py
@@ -0,0 +1,23 @@
+# writing comments in python
+# A program is a set of instructions given to a computer.
+# integers
+age = 40
+marks = 500
+points = 8000
+height = 56
+
+# floats
+weight = 78.5
+speed = 100.2
+# strings
+county = "Kisii"
+name = "Joseph"
+
+print(age)
+print(marks)
+print(points)
+print(height)
+print(weight)
+print(speed)
+print(county)
+print(name)
